337c56c267effc2a068a261c320b507f0aaf0c03
dark0ghost-vivobook <anonduckduck@duck.com>
2022-02-20 19:14:10 +0300

    added test for Palindrome

---
+++ src/test/kotlin/org.dark0ghost.lab2.test/PalindromeTest.kt
@@ -0,0 +1,20 @@
+package org.dark0ghost.lab2.test
+
+import org.dark0ghost.lab2.point.isPalindrome
+import org.junit.jupiter.api.Test
+
+class PalindromeTest {
+
+    @Test
+    fun testPalindrome() {
+        val testPalindrome1 = "lol"
+        assert(isPalindrome(testPalindrome1))
+
+        val testPalindrome2 = "121"
+        assert(isPalindrome(testPalindrome2))
+
+        val testPalindrome3 = "122"
+        assert(!isPalindrome(testPalindrome3))
+
+    }
+}
