7777079f5b6800601ce40a725ac00f08f1184bf4
dark0ghost <dark0ghostworkmail@seznam.cz>
2022-03-03 23:35:26 +0300

    added ui component

--- src/main/kotlin/org/dark0ghost/lab4/JImageDisplay.kt
+++
@@ -1,9 +0,0 @@
-package org.dark0ghost.lab4
-
-import com.intellij.ide.actions.WindowAction.DecrementHeight
-import java.awt.image.BufferedImage
-import javax.swing.JComponent
-
-class JImageDisplay(private val height: UInt, private val weight: UInt): JComponent() {
-    private lateinit var bufferedImage: BufferedImage
-}
---
+++ src/main/kotlin/org/dark0ghost/lab4/fractal/Mandelbrot.kt
@@ -0,0 +1,8 @@
+package org.dark0ghost.lab4.fractal
+
+class Mandelbrot {
+
+    companion object {
+        const val MAX_ITERATIONS = 2000
+    }
+}
